8fceb081f25016f6f607939904c6fe3db4647bfa
Jossue Sanabria <digitalnomadwebdeveloper@gmail.com>
2023-01-03 09:32:57 -0600

    Update Client-service
    Actualizacion

--- service/client-service.js
+++ service/client-service.js
@@ -41,23 +41,7 @@ const table = document.querySelector("[data-table]");
 //Delete -- DELETE
 
 const listaClientes = () => {
-  const promise = new Promise ( (resolve, reject) => {
-    const http = new XMLHttpRequest();
-
-      http.open("GET","http://localhost:3000/perfil");
-
-      http.send();
-
-      http.onload = () => {
-        const response = JSON.parse(http.response);
-        if (http.status>=400){
-          reject(response)
-        }else{
-          resolve(response)
-        }
-      };
-  });
-  return promise;
+  return fetch("http://localhost:3000/perfil")
 };
 
 listaClientes ()
