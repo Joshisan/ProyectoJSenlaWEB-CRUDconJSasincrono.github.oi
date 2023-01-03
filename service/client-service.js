5b6d6377868f2c29a7f2be84bea36ea29e20ea69
Jossue Sanabria <digitalnomadwebdeveloper@gmail.com>
2023-01-03 13:47:16 -0600

    Commit Changes
    Files Update

--- service/client-service.js
+++ service/client-service.js
@@ -1,38 +1,3 @@
-const crearNuevaLinea = (nombre,email) => {
-    const linea = document.createElement("tr");
-    const contenido = `
-    <td class="td" data-td>
-    ${nombre}
-    </td>
-    <td>
-    ${email}
-    </td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-            >Editar</a
-          >
-        </li>
-        <li>
-          <button
-            class="simple-button simple-button--delete"
-            type="button"
-          >
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  `;
-  linea.innerHTML = contenido;
-  return linea;
-}
-
-const table = document.querySelector("[data-table]");
-
 //Abrir http(metodo,url)
 //CRUD -- Metodos HTTP
 //Create -- POST
@@ -40,18 +5,26 @@ const table = document.querySelector("[data-table]");
 //Update -- PUT/PATCH
 //Delete -- DELETE
 
-const listaClientes = () => {
-  return fetch("http://localhost:3000/perfil");
+//Fetch API
+const listaClientes = () =>
+ fetch("http://localhost:3000/perfil").then(respuesta => respuesta.json());
+
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      nombre,
+      email,
+      id: uuid.v4()
+    })
+  })
 };
 
-listaClientes ()
-.then ((data) => {
-  data.forEach(perfil => {
-    const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-    table.appendChild(nuevaLinea);
-  });
-})
-.catch((error) => alert("¡Ocurrió un error!"))
-
-
+export const clientServices = {
+  listaClientes,
+  crearCliente
+};
 
