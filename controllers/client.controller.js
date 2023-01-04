7287bcd7007f8a6593099effa30825569b9d0a54
Jossue Sanabria <digitalnomadwebdeveloper@gmail.com>
2023-01-04 15:39:12 -0600

    Commit Updates

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,19 +1,19 @@
 import { clientServices } from "../service/client-service.js";
 
+//backticks
 const crearNuevaLinea = (nombre, email, id) => {
-    const linea = document.createElement("tr");
-    const contenido = `
+  const linea = document.createElement("tr");
+  const contenido = `
     <td class="td" data-td>
-    ${nombre}
+      ${nombre}
     </td>
-    <td>
-    ${email}
-    </td>
+    <td>${email}</td>
     <td>
       <ul class="table__button-control">
         <li>
-          <a href="../screens/editar_cliente.html?${id}" 
-          class="simple-button simple-button--edit"
+          <a
+            href="../screens/editar_cliente.html?id=${id}"
+            class="simple-button simple-button--edit"
           >
             Editar
           </a>
@@ -26,29 +26,29 @@ const crearNuevaLinea = (nombre, email, id) => {
       </ul>
     </td>
   `;
-  
   linea.innerHTML = contenido;
   const btn = linea.querySelector("button");
   btn.addEventListener("click", () => {
     const id = btn.id;
     clientServices
       .eliminarCliente(id)
-      .then(respuesta =>{
+      .then((respuesta) => {
         console.log(respuesta);
       })
-      .catch(err => alert("Ocurrió un error"));
+      .catch((err) => alert("Ocurrió un error"));
   });
+
   return linea;
 };
 
 const table = document.querySelector("[data-table]");
 
 clientServices
-.listaClientes ()
-.then ((data) => {
-  data.forEach(({nombre, email, id}) => {
-    const nuevaLinea = crearNuevaLinea(nombre, email, id);
-    table.appendChild(nuevaLinea);
-  });
-})
-.catch((error) => alert("¡Ocurrió un error!"));
+  .listaClientes()
+  .then((data) => {
+    data.forEach(({ nombre, email, id }) => {
+      const nuevaLinea = crearNuevaLinea(nombre, email, id);
+      table.appendChild(nuevaLinea);
+    });
+  })
+  .catch((error) => alert("Ocurrió un error"));
